d6ceb0fe3b37bf24911a0cd93f20e38ebcee7e4c
azhaguprabu <azhaguprabu@gmail.com>
2017-11-16 13:09:10 -0500

    auto_key working

--- Shell/scripts/auto_key.sh
+++ Shell/scripts/auto_key.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 function make_script() {
-	sed 'i/\[\[key\]\]/$1/g' remote_run.sh > remote_run_$2.sh 	
+	eval key=$1
+	sed "s/key_paste/$key/" remote_run.sh > remote_run_$2.sh
 }
 
-key="ajsdklfjaklsjdfkajsdlf jaksdjfklajsdfjaklsjdfklasjdfkljasdkfjklasdjflkasjdfkasjdfkajsf"
+key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBYDfhbwJXcR9RWr6A3A5/zOIOtxmvYnrwzjMzMfM7DRGdjFSOcePY1upTTotMFwK8QMx5hC+/zCjuQkp+z1WiSVC2eLWmEdEw5Rw8MFGZnTc2HF4Su/m7flB3JpRG/8jTTNmqd4Ru8qGvbGPPbgtdGtnIeNErUpH46EepXy6X4vJkZnTpBP/CiPFAg4rbmOVQIQuuyxtCZT3MTu49ot87N8THgBNDttjIa3J2ORTUGlMJmMjeO1ULxIorFtjtvMiD7KlnmJMqsWVJ0QFceI4uCKc5C3X58wU9bfGB1jWR2rTRWAanPYSNwONZb+oL1HApTzuB+2nRfbkC590iXpPV root@server.example.com"
 
-make_template $key $1
+echo $key > key_temp.txt
 
-scp remote_run_$1.sh azjayaram@$2:~/
+sed -i 's/\//\\\//g' key_temp.txt
+key=`cat key_temp.txt`
+echo $key
+make_script "\${key}" $1
 
-ssh azjayaram@$2 '/bin/bash ~/remote_run_$1.sh'
+scp remote_run_$1.sh $2@$1:~/
+
+ssh $2@$1 "/bin/bash ~/remote_run_$1.sh"
--- Shell/scripts/remote_run.sh
+++ Shell/scripts/remote_run.sh
@@ -6,9 +6,7 @@ else
 	mkdir -v ~/.ssh
 	chmod 700 ~/.ssh
 fi
-
 echo "Paste the key"
-key="[[key_Paste]]"
-
-echo "$key" >> ~./ssh/authorized_keys && echo "Authorized Key Pasted"
+key="key_paste"
+echo "$key" >> ~/.ssh/authorized_keys && echo "Authorized Key Pasted"
 chmod 600 ~/.ssh/authorized_keys && echo "permission given Authorized_keys"
